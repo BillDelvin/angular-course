ed14fefb13fd0075a17aa4dbd3139cdcd2e7e516
Bill Delvin <billdelvin6@gmail.com>
2020-08-27 11:00:31 +0700

    inject recipes service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipes } from "../recipes.model";
+import { RecipesService } from "../recipes.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipes;
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddToShoppingList() {
+    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
