ebb91c70b97a96a1b492fd5210cd3a8450bb7784
Bill Delvin <billdelvin6@gmail.com>
2020-08-27 11:09:33 +0700

    inject service inside shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "./shopping-list.service";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"],
 })
 export class ShoppingListComponent implements OnInit {
+  constructor(private slService: ShoppingListService) {}
 
-  constructor() { }
+  ingredients: Ingredient[];
 
   ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientChanged.subscribe((ingredient: Ingredient[]) => {
+      this.ingredients = ingredient;
+    });
   }
-
 }
