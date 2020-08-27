b9950fc3b0d9d915e310eb875ae0d5e494a41259
Bill Delvin <billdelvin6@gmail.com>
2020-08-27 10:54:56 +0700

    clean code

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipes } from "../../recipes.model";
+import { RecipesService } from "../../recipes.service";
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  selector: "app-recipe-item",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.css"],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipes;
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect() {
+    this.recipesService.recipesSelected.emit(this.recipe);
   }
-
 }
