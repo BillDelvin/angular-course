aa716f9626b4258cf24287da6280d29ba85b6a31
Bill Delvin <billdelvin6@gmail.com>
2020-08-28 10:08:05 +0700

    make childRoute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
@@ -13,6 +13,10 @@ const routes: Routes = [
   {
     path: "recipes",
     component: RecipesComponent,
+    children: [
+      { path: "", component: RecipeStartComponent },
+      { path: ":id", component: RecipeDetailComponent },
+    ],
   },
   { path: "shopping-list", component: ShoppingListComponent },
 ];
