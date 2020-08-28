0aa7ef8c6587009494a09028c4092566b402bcd7
Bill Delvin <billdelvin6@gmail.com>
2020-08-28 11:26:15 +0700

    adding recipe edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: "full" },
@@ -15,7 +15,9 @@ const routes: Routes = [
     component: RecipesComponent,
     children: [
       { path: "", component: RecipeStartComponent },
+      { path: "new", component: RecipeEditComponent },
       { path: ":id", component: RecipeDetailComponent },
+      { path: ":id/edit", component: RecipeEditComponent },
     ],
   },
   { path: "shopping-list", component: ShoppingListComponent },
