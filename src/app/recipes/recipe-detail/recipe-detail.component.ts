ada35e8c554cc5b08ee3e82edb2050d98c91782b
Bill Delvin <billdelvin6@gmail.com>
2020-08-28 10:04:32 +0700

    inject service and adding activateRoute

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+
 import { Recipes } from "../recipes.model";
 import { RecipesService } from "../recipes.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-recipe-detail",
   templateUrl: "./recipe-detail.component.html",
   styleUrls: ["./recipe-detail.component.css"],
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipes;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  constructor(
+    private recipesService: RecipesService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor(private recipesService: RecipesService) {}
+  recipe: Recipes;
+  id: number;
+  recipeSubs: Subscription;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipeSubs = this.route.params.subscribe((param: Params) => {
+      this.id = +param["id"];
+      this.recipe = this.recipesService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  ngOnDestroy() {
+    this.recipeSubs.unsubscribe();
   }
 }
