2d2beb3f9a9c9fe840734b99eeafc9cc9dc44a66
Bill Delvin <billdelvin6@gmail.com>
2020-09-01 09:30:31 +0700

    make subscribetion for trigger change

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Recipes } from "../recipes.model";
 import { RecipesService } from "../recipes.service";
 import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html",
   styleUrls: ["./recipe-list.component.css"],
 })
-export class RecipeListComponent implements OnInit {
-  recipes: Recipes[];
-
+export class RecipeListComponent implements OnInit, OnDestroy {
   constructor(
     private recipesService: RecipesService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
+  recipes: Recipes[];
+  recipeChanged: Subscription;
+
   ngOnInit(): void {
+    this.recipeChanged = this.recipesService.recipeChanged.subscribe(
+      (recipes: Recipes[]) => {
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipesService.getRecipes();
   }
 
   onNewRecipe() {
     this.router.navigate(["new"], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.recipeChanged.unsubscribe();
   }
 }
