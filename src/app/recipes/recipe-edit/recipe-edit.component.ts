b8656872e3f3ac42d1099a6291321bb18f92021f
Bill Delvin <billdelvin6@gmail.com>
2020-09-01 08:41:59 +0700

    create reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Subscription } from "rxjs";
+import { FormGroup, FormControl, FormArray } from "@angular/forms";
+import { RecipesService } from "../recipes.service";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
   selector: "app-recipe-edit",
@@ -8,18 +11,61 @@ import { Subscription } from "rxjs";
   styleUrls: ["./recipe-edit.component.css"],
 })
 export class RecipeEditComponent implements OnInit, OnDestroy {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipesService
+  ) {}
 
   id: number;
   editMode = false;
   paramSubs: Subscription;
+  recipeForm: FormGroup;
 
   ngOnInit(): void {
     this.paramSubs = this.route.params.subscribe((params: Params) => {
       this.id = +params["id"];
       this.editMode = params["id"] != null;
-      console.log(this.editMode);
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+    let recipeIngredient = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredient.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredient,
+    });
+  }
+
+  get ingredients() {
+    return <FormArray>this.recipeForm.get("ingredients");
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
   ngOnDestroy() {
