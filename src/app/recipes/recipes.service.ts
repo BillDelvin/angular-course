26002a9b0d020689562393619d5321817ba689f0
Bill Delvin <billdelvin6@gmail.com>
2020-09-04 09:52:33 +0700

    change get recipes from firebase

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -12,20 +12,27 @@ export class RecipesService {
 
   recipeChanged = new Subject<Recipes[]>();
 
-  private recipes: Recipes[] = [
-    new Recipes(
-      "A Test Recipe",
-      "This is simple a test",
-      "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
-      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
-    ),
-    new Recipes(
-      "A Test Recipe 1",
-      "This is Advanced a test",
-      "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
-      [new Ingredient("Bakso", 2), new Ingredient("Mie Goreng", 2)]
-    ),
-  ];
+  // private recipes: Recipes[] = [
+  // new Recipes(
+  //   "A Test Recipe",
+  //   "This is simple a test",
+  //   "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
+  //   [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
+  // ),
+  // new Recipes(
+  //   "A Test Recipe 1",
+  //   "This is Advanced a test",
+  //   "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
+  //   [new Ingredient("Bakso", 2), new Ingredient("Mie Goreng", 2)]
+  // ),
+  // ];
+
+  private recipes: Recipes[] = [];
+
+  setRecipes(recipes: Recipes[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     // slice() with empty argument it return the copy of recipes
