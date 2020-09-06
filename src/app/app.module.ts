79d20fd6adc5166bd7c079b05d80557c9e2ee40e
Bill Delvin <billdelvin6@gmail.com>
2020-09-06 13:18:59 +0700

    make programtic handle error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from "./auth/auth.component";
 import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
+import { AlertComponent } from "./shared/alert/alert.component";
+import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive";
 
 @NgModule({
   declarations: [
@@ -35,6 +37,8 @@ import { AuthInterceptorService } from "./auth/auth-interceptor.service";
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceHolderDirective,
   ],
   imports: [
     BrowserModule,
@@ -53,5 +57,6 @@ import { AuthInterceptorService } from "./auth/auth-interceptor.service";
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  entryComponents: [AlertComponent],
 })
 export class AppModule {}
