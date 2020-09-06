5bed889c85722aea9bea29b1eed70b76b80d5fec
Bill Delvin <billdelvin6@gmail.com>
2020-09-06 13:18:16 +0700

    make programmatic handel error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,30 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ComponentFactoryResolver,
+  ViewChild,
+  OnDestroy,
+} from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceHolderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
   styleUrls: ["./auth.component.css"],
 })
-export class AuthComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AuthComponent implements OnInit, OnDestroy {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceHolderDirective, { static: false })
+  alertHost: PlaceHolderDirective;
+  private closeSub: Subscription;
 
   ngOnInit(): void {}
 
   onSwithcMode() {
     this.isLoginMode = !this.isLoginMode;
   }
-
-  // onClearError() {
-  //   this.error = null;
-  // }
 
   onSubmit(form: NgForm) {
     if (!form.valid) {
@@ -49,10 +60,35 @@ export class AuthComponent implements OnInit {
       },
       (errorMessage) => {
         this.error = errorMessage;
+        this.showAlertError(errorMessage);
         this.isLoading = false;
       }
     );
 
     form.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showAlertError(message: string) {
+    const alertComponent = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear(); // this tell angular i want clear that component before i render it
+    const componentRef = hostViewContainerRef.createComponent(alertComponent);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 }
