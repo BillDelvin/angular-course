c35e41a179c739e312ce90daa7b5d0a55bd5bc9d
billdelvin <billdelvin6@gmail.com>
2020-09-07 20:25:25 +0700

    create shopping routing and module

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+
+import { ShoppingListComponent } from "./shopping-list.component";
+import { AuthGuard } from "../auth/auth.guard";
+
+const routes: Routes = [
+  {
+    path: "shopping-list",
+    component: ShoppingListComponent,
+    canActivate: [AuthGuard],
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ShoppingListRoutingModule {}
