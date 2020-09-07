52f2fedc875b62995b671726677324e1e2b1dc8e
billdelvin <billdelvin6@gmail.com>
2020-09-07 23:52:41 +0700

    adding auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { CoreModule } from "./core.module";
+import { AuthModule } from "./auth/auth.module";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { AuthComponent } from "./auth/auth.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
+    AuthModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
-    ReactiveFormsModule,
-    FormsModule,
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
