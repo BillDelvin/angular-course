bdc54d7a1108dba5dd2389bcf99d54fe49eaef2c
billdelvin <billdelvin6@gmail.com>
2020-09-07 21:06:25 +0700

    adding shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,27 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AuthComponent } from "./auth/auth.component";
-import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
-import { AlertComponent } from "./shared/alert/alert.component";
-import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceHolderDirective,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule,
     ReactiveFormsModule,
     FormsModule,
-    NgbModule,
   ],
   providers: [
     {
@@ -43,6 +33,5 @@ import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  entryComponents: [AlertComponent],
 })
 export class AppModule {}
