07f0d63309a423d69cd91f7b1808edcc0b2666c8
billdelvin <billdelvin6@gmail.com>
2020-09-07 20:30:54 +0700

    split routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { AuthComponent } from "./auth/auth.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: "full" },
