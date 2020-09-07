99e834792c6ee883520f292abd8b5c065109973b
billdelvin <billdelvin6@gmail.com>
2020-09-07 20:44:26 +0700

    remove dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { DropdownDirective } from "./shared/dropdown.directive";
 import { AuthComponent } from "./auth/auth.component";
 import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
@@ -20,7 +19,6 @@ import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
