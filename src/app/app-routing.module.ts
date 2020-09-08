0e8527767e6f47b3913556c03e77eebd73119d39
billdelvin <billdelvin6@gmail.com>
2020-09-08 21:58:04 +0700

    make more optimize using preload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
-const routes: Routes = [
+const appRoutes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: "full" },
   {
     path: "recipes",
@@ -22,7 +22,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
