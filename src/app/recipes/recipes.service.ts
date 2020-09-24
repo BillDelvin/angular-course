fc86264bcfbf2cb6458d22067c18ebd89e53140d
billdelvin <billdelvin6@gmail.com>
2020-09-24 21:31:34 +0700

    clean code

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -12,21 +12,6 @@ export class RecipesService {
 
   recipeChanged = new Subject<Recipes[]>();
 
-  // private recipes: Recipes[] = [
-  // new Recipes(
-  //   "A Test Recipe",
-  //   "This is simple a test",
-  //   "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
-  //   [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
-  // ),
-  // new Recipes(
-  //   "A Test Recipe 1",
-  //   "This is Advanced a test",
-  //   "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&h=350",
-  //   [new Ingredient("Bakso", 2), new Ingredient("Mie Goreng", 2)]
-  // ),
-  // ];
-
   private recipes: Recipes[] = [];
 
   setRecipes(recipes: Recipes[]) {
